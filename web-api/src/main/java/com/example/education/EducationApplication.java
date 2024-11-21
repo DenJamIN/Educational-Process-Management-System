68d638c8fe277fdc0fa82f396457a823683d5d83
denjamin-loverentev <denlavre@mail.ru>
2024-11-21 21:29:13 +0300

    локальное хранилище (не работает шедулер)

--- web-api/src/main/java/com/example/education/EducationApplication.java
+++ web-api/src/main/java/com/example/education/EducationApplication.java
@@ -2,8 +2,10 @@ package com.example.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EducationApplication {
 
 	public static void main(String[] args) {
