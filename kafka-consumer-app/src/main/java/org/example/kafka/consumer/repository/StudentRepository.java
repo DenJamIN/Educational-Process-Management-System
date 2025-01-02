8fa20eeec66cc3df361ba3248b8cfb75e9a86dc7
denjamin-loverentev <denlavre@mail.ru>
2025-01-02 16:58:13 +0300

    миграция кафки и спарка на питон

--- kafka-consumer-app/src/main/java/org/example/kafka/consumer/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.kafka.consumer.repository;
-
-import org.example.kafka.consumer.model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends MongoRepository<Student, Long> {
-}
---
+++ kafka_consumer/consumer.py
@@ -0,0 +1,31 @@
+from kafka import KafkaConsumer
+from pymongo import MongoClient
+import json
+
+# Настройки Kafka
+KAFKA_BOOTSTRAP_SERVERS = 'kafka:9092'
+KAFKA_TOPIC = 'education_topic'
+GROUP_ID = 'education_consumer_group'
+
+# Настройки MongoDB
+MONGO_URI = 'mongodb://mongo:27017/'
+MONGO_DB = 'education_db'
+MONGO_COLLECTION = 'courses'
+
+# Подключение к MongoDB
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
+collection = db[MONGO_COLLECTION]
+
+# Подключение к Kafka
+consumer = KafkaConsumer(
+    KAFKA_TOPIC,
+    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+    group_id=GROUP_ID,
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+for message in consumer:
+    data = message.value
+    collection.insert_one(data)
+    print(f"Inserted document: {data}")
